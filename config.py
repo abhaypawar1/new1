8f02405c28cf325f4568129e07f53b1da2c9ea73
abhaypawar1 <abhaypawar1198@gmail.com>
2023-02-28 20:44:26 +0530

    new file

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+modelpath=os.path.join ("E:\DATA SCIENCE\GITHUB\project_2\projectknn","model.pkl")
+jsonpath=os.path.join("E:\DATA SCIENCE\GITHUB\project_2\projectknn","jsonfile.json")
+port_no=5005
